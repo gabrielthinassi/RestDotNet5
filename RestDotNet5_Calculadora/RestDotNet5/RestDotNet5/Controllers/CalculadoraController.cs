4cc8cc69ae5aca20b80b72331c4ec3c3abb866f1
gabrielthinassi <gabriel.m.thinassi@gmail.com>
2022-02-08 14:47:17 -0300

    'calculadora'

---
+++ RestDotNet5_Calculadora/RestDotNet5/RestDotNet5/Controllers/CalculadoraController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestDotNet5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculadoraController : ControllerBase
+    {
+        private readonly ILogger<CalculadoraController> _logger;
+
+        public CalculadoraController(ILogger<CalculadoraController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Valores inválidos!");
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalNumber;
+            if(decimal.TryParse(strNumber, out decimalNumber)) {
+                return decimalNumber;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strNumber, 
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+            return isNumber;
+        }
+    }
+}
